d4ee281d6e9b6791de6cca61221e24eba7d765cf
adeelbutt <adeel@codingcrafts.io>
2023-12-17 11:55:17 +0500

    added env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return `Hello World! and port is ${process.env.PORT}`;
+    const port = this.configService.get<number>('PORT') || 3000;
+    return `Hello World! Server is running on port ${port}`;
   }
 }
