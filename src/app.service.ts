37c61d5797be1602b08bfa8c6db96806b6cb62bc
adeelbutt <adeel@codingcrafts.io>
2023-12-17 12:20:23 +0500

    changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,6 @@ export class AppService {
   getHello(): string {
     const port = this.configService.get<number>('PORT') || 3000;
     const TEST_ENV = this.configService.get<string>('TEST_ENV') || 'testenv2';
-    return `Hello World! Server is running on port ${port} and testenv value is : ${TEST_ENV}`;
+    return `Hello World!!!! Server is running on port ${port} and testenv value is : ${TEST_ENV}`;
   }
 }
